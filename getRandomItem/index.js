e77f5049f09bf7ce3737c79309de26d362c0e2f1
Dean Bryen <deanbryen@hotmail.com>
2019-01-22 04:10:49 +0200

    init

---
+++ getRandomItem/index.js
@@ -0,0 +1,10 @@
+
+const axios = require('axios')
+module.exports = async function (context, req) {
+
+
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Hello Tel Aviv"
+        };
+};
