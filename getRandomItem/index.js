a36fae23b272b036204a70a2a5d6b73a68815080
Dean Bryen <deanbryen@hotmail.com>
2019-01-22 04:31:11 +0200

    people

--- getRandomItem/index.js
+++ getRandomItem/index.js
@@ -17,6 +17,6 @@ module.exports = async function (context, req) {
 
          context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello London"
+            body: "Hello London people"
         };
 };
