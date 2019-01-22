455d4c300133ac2bab1250bcff36f325444cd2e5
Dean Bryen <deanbryen@hotmail.com>
2019-01-22 04:20:20 +0200

    london

--- getRandomItem/index.js
+++ getRandomItem/index.js
@@ -2,9 +2,21 @@
 const axios = require('axios')
 module.exports = async function (context, req) {
 
+  /*   axios.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  }); */
 
-        context.res = {
+         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello Tel Aviv"
+            body: "Hello London"
         };
 };
